65a5aded7543a7a946d98f89bfb42c84a97b720f
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-01 11:29:51 -0700

    Moving project over to a new repo Exchange_Rate This new repo is functional and does not return the same failed to fetch erros for unknown reasons

--- src/index.js
+++ src/index.js
@@ -2,28 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import rateCheck from './js/rateCheck';
+import RateCheck from './js/rates.js';
 
 function getElements(response) {
-    if (response.conversion_result) {
-      $('.output').append(`<p>${response.conversion_result}</p>`);
-      
-    } else {
-        $('.output').append(`<p>There was an error: ${response}</p>`);
-    }
+  if (response.result) {
+    $('#test').text(`The test is as follows ${response.conversion_rates}`);
+    
+  } else {
+    $('#test2').text(`There was an error: ${response}`);
   }
+}
 
-async function makeApiCall(country1, country2, amount) {
-    const response = await rateCheck.getRates(country1, country2, amount);
-    console.log(response);
-    getElements(response);
+async function makeApiCall() {
+  const response = await RateCheck.getRate();
+  getElements(response);
 }
 
 $(document).ready(function() {
-    $('#form').submit(function() {
-        const country1 = $('#currency1').val();
-        const country2 = $('#currency2').val();
-        const amount = $('#amount').val();
-        makeApiCall(country1, country2, amount);
-    });
+  $('#form').click(function() {
+   
+    makeApiCall('USD', 'EUR', 5);
   });
+});
