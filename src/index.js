6991f8bd653f71ae77f49993cb837c1f6fff86af
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-01 09:03:57 -0700

    added functions to recieve api output and began writing asynch function

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Rectangle from './rectangle.js';
+import rateCheck from './rateCheck.js';
+
+function getElements(response) {
+    if (response.result === "success") {
+      console.log(`${response.conversion_result} `);
+      
+    } else {
+      $('.showErrors').text(`There was an error: ${response}`);
+    }
+  }
